c5b41765ef05a80f045051e91c6d3979555ff5d4
Xiaomin <xiaomin.chu.numbertheory@gmail.com>
2023-12-06 20:42:19 +0100

    convenient snippet writing

--- LuaSnip/lua/snippet_convention.lua
+++ LuaSnip/lua/snippet_convention.lua
@@ -20,5 +20,18 @@ return {s(
         local d = ls.dynamic_node
         local fmt = require("luasnip.extras.fmt").fmta
         local rep = require("luasnip.extras").rep
+
+        local snippets = {}
+
     ]], {})
-)}
+),
+    s(
+    {trig="snins"},
+    fmt([[
+    table.insert(snippets, s(
+        {trig = <>, <>},
+        fmt(<>, {<>})
+    ))
+
+    ]], {i(1), c(2, {t(""), t("snippetType=\"autosnippet\"")}), i(3), i(4)}))
+}
